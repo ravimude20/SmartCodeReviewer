d30bb6d868493a740557670a84501cb34b2e5030
Ravi Ganpat Mude <ravimude20@gmail.com>
2023-08-20 01:14:19 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -151,7 +151,7 @@ function getAIResponse(prompt) {
     var _a, _b;
     return __awaiter(this, void 0, void 0, function* () {
         const queryConfig = {
-            model: "gpt-4",
+            model: "gpt-3.5-turbo",
             temperature: 0.2,
             max_tokens: 700,
             top_p: 1,
