a3ff3c3fba17469e605a61a086c20a7a07dee81a
Dikshant Agarwal <contact2dikshant@gmail.com>
2023-08-23 12:33:44 +0530

    Update prompts

--- dist/index.js
+++ dist/index.js
@@ -122,10 +122,15 @@ function createPrompt(file, chunk, prDetails) {
     return `Your task is to review pull requests. Instructions:
 - Provide the response in following JSON format:  [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]
 - Do not give positive comments or compliments.
+- Point out bad code practices.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise return an empty array.
 - Write the comment in GitHub Markdown format.
 - Use the given description only for the overall context and only comment the code.
 - IMPORTANT: NEVER suggest adding comments to the code.
+- IMPORTANT: Suggest best practises code snippet at the end.
+- Provide reference of best practices documentation if any.
+- Review lint errors.
+- Prefix the start line of review with "AI Code Review:"
 
 Review the following code diff in the file "${file.to}" and take the pull request title and description into account when writing the response.
   
