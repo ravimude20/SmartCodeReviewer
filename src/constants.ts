f7816d2d35918d2943c0ace6c57a75af92c23afe
Dikshant Agarwal <contact2dikshant@gmail.com>
2023-08-23 12:11:16 +0530

    refactor code and udate tokens

--- src/constants.ts
+++
@@ -1,5 +0,0 @@
-import * as core from "@actions/core";
-
-export const GITHUB_TOKEN: string = core.getInput("GITHUBTOKEN");
-export const OPENAI_API_KEY: string = core.getInput("OPENAI_API_KEY");
-export const OPENAI_API_MODEL: string = core.getInput("OPENAI_API_MODEL");
